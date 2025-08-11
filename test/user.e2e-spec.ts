af9bd3191a96631c8104bc083dc7553b0308c895
sergey <sosninsd@gmail.com>
2025-08-11 18:50:02 +0300

    1. admin registration is added 2. admin e2e tests are added

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -2,12 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { JwtService } from '@nestjs/jwt';
 
 import * as cookieParser from 'cookie-parser';
 import { timeout } from 'rxjs';
 
-const jwtService = new JwtService();
+interface User {
+  email: string;
+  password: string;
+  accessToken: string;
+  cookies: string;
+  id: number;
+  isAdmin: boolean;
+}
 
 describe('UsersController (e2e)', () => {
   let app: INestApplication;
@@ -32,61 +38,38 @@ describe('UsersController (e2e)', () => {
     await app.close();
   });
 
-  let accessToken: string;
-  let adminAccessToken: string;
-  let cookies: string;
-
-  const email = 'test_2@email.com';
-  const adminEmail = 'teat_admin@email.com';
-  const password = 'password';
+  let nonAdminUser: User = {
+    email: 'test_non_admin3@email.com',
+    password: 'password',
+    accessToken: '',
+    cookies: '',
+    id: 0,
+    isAdmin: false,
+  };
+  let adminUser: User = {
+    email: 'test_admin2@email.com',
+    password: 'password',
+    accessToken: '',
+    cookies: '',
+    id: 0,
+    isAdmin: true,
+  };
 
   it('/users/register (POST) should register a user', async () => {
-    const response = await request(httpServer)
-      .post('/users/register')
-      .send({
-        email: email,
-        password: password,
-      })
-      .expect(201);
-
-    expect(response.body).toHaveProperty('id');
+    nonAdminUser = await registerUser(nonAdminUser, httpServer);
   });
 
   it('/users/login (POST) should log in a user', async () => {
-    const response = await request(httpServer)
-      .post('/users/login')
-      .send({
-        email: email,
-        password: password,
-      })
-      .expect(201);
-
-    expect(response.body).toHaveProperty('accessToken');
-    accessToken = response.body.accessToken;
-
-    cookies = response.headers['set-cookie'][0];
-    expect(cookies).toBeDefined();
+    nonAdminUser = await loginUser(nonAdminUser, httpServer);
   });
 
   it('/users/refresh (POST) should refresh access token for a user', async () => {
-    const response = await request(httpServer)
-      .post('/users/refresh')
-      .set('Cookie', `${cookies}`)
-      // TODO: what status should be here?
-      .expect(201);
-
-    expect(response.body).toHaveProperty('accessToken');
-    accessToken = response.body.accessToken;
-
-    cookies = response.headers['set-cookie'][0];
-    expect(cookies).toBeDefined();
+    nonAdminUser = await refreshToken(nonAdminUser, httpServer);
   });
 
   it('/users/:id (GET) should get user', async () => {
-    const userId = jwtService.decode(accessToken).sub;
-
     const response = await request(httpServer)
-      .get(`/users/${userId}`)
+      .get(`/users/${nonAdminUser.id}`)
       .expect(200);
 
     expect(response.body).toBeDefined();
@@ -95,7 +78,7 @@ describe('UsersController (e2e)', () => {
   it('/users/ (GET) non-admin user should have 403 on get all users', async () => {
     const response = await request(httpServer)
       .get(`/users/`)
-      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Authorization', `Bearer ${nonAdminUser.accessToken}`)
       .expect(403);
 
     expect(response.body).toBeDefined();
@@ -110,59 +93,94 @@ describe('UsersController (e2e)', () => {
   });
 
   it('/users/:id (DELETE) should delete a user (authorized)', async () => {
-    const userId = jwtService.decode(accessToken).sub;
+    await deleteUser(nonAdminUser, httpServer);
+  });
 
+  it('/users/register (POST) should register a user as admin', async () => {
+    adminUser = await registerUser(adminUser, httpServer);
+  });
+
+  it('/users/login (POST) should log in a user as admin', async () => {
+    adminUser = await loginUser(adminUser, httpServer);
+  });
+
+  it('/users/:id (GET) admin user should a user', async () => {
     const response = await request(httpServer)
-      .delete(`/users/${userId}`)
-      .set('Authorization', `Bearer ${accessToken}`)
+      .get(`/users/${adminUser.id}`)
+      .set('Authorization', `Bearer ${adminUser.accessToken}`)
       .expect(200);
 
     expect(response.body).toBeDefined();
   });
 
-  it('/users/register (POST) should register a user as admin', async () => {
-    const response = await request(httpServer)
-      .post('/users/register')
-      .send({
-        email: adminEmail,
-        password: password,
-        isAdmin: true,
-      })
-      .expect(201);
-
-    expect(response.body).toHaveProperty('id');
-  });
-
-  it('/users/login (POST) should log in a user as admin', async () => {
-    const response = await request(httpServer)
-      .post('/users/login')
-      .send({
-        email: adminEmail,
-        password: password,
-      })
-      .expect(201);
-
-    expect(response.body).toHaveProperty('accessToken');
-    adminAccessToken = response.body.accessToken;
-  });
-
   it('/users/ (GET) admin user should have all users', async () => {
     const response = await request(httpServer)
       .get(`/users/`)
-      .set('Authorization', `Bearer ${adminAccessToken}`)
+      .set('Authorization', `Bearer ${adminUser.accessToken}`)
       .expect(200);
 
     expect(response.body).toBeDefined();
   });
 
   it('/users/:id (DELETE) should delete an admin user (authorized)', async () => {
-    const userId = jwtService.decode(adminAccessToken).sub;
+    await deleteUser(adminUser, httpServer);
+  });
 
+  async function registerUser(user: User, httpServer: any): Promise<User> {
     const response = await request(httpServer)
-      .delete(`/users/${userId}`)
-      .set('Authorization', `Bearer ${adminAccessToken}`)
+      .post('/users/register')
+      .send({
+        email: user.email,
+        password: user.password,
+        isAdmin: user.isAdmin,
+      })
+      .expect(201);
+
+    expect(response.body).toHaveProperty('id');
+    user.id = response.body.id;
+    return user;
+  }
+
+  async function loginUser(user: User, httpServer: any): Promise<User> {
+    const response = await request(httpServer)
+      .post('/users/login')
+      .send({
+        email: user.email,
+        password: user.password,
+      })
+      .expect(201);
+
+    expect(response.body).toHaveProperty('accessToken');
+    user.accessToken = response.body.accessToken;
+
+    user.cookies = response.headers['set-cookie'][0];
+    expect(user.cookies).toBeDefined();
+
+    return user;
+  }
+
+  async function refreshToken(user: User, httpServer: any): Promise<User> {
+    const response = await request(httpServer)
+      .post('/users/refresh')
+      .set('Cookie', `${user.cookies}`)
+      // TODO: what status should be here?
+      .expect(201);
+
+    expect(response.body).toHaveProperty('accessToken');
+    user.accessToken = response.body.accessToken;
+
+    user.cookies = response.headers['set-cookie'][0];
+    expect(user.cookies).toBeDefined();
+
+    return user;
+  }
+
+  async function deleteUser(user: User, httpServer: any) {
+    const response = await request(httpServer)
+      .delete(`/users/${user.id}`)
+      .set('Authorization', `Bearer ${user.accessToken}`)
       .expect(200);
 
     expect(response.body).toBeDefined();
-  });
+  }
 });
