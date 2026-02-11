8b4b5769643e16b457073814faa2600b431ce28b
dev-un <149966953+dev-un@users.noreply.github.com>
2026-02-11 23:59:50 +0500

    clear subscription.controller.ts

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -1,27 +1,11 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
-import { CreateSubscriptionDto } from './dto/create-subscription.dto';
-import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
 import { UserId } from '../../decorators/user-id.decorator';
 import { AuthGuard } from '../../guards/auth.guard';
 
 @Controller('subscriptions')
 export class SubscriptionController {
   constructor(private readonly subscriptionService: SubscriptionService) {}
-
-  @Post()
-  create(@Body() createSubscriptionDto: CreateSubscriptionDto) {
-    return this.subscriptionService.create(createSubscriptionDto);
-  }
 
   @Get()
   getAll() {
@@ -37,14 +21,6 @@ export class SubscriptionController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.subscriptionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateSubscriptionDto: UpdateSubscriptionDto,
-  ) {
-    return this.subscriptionService.update(+id, updateSubscriptionDto);
   }
 
   @Delete(':id')
